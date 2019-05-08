125deb7c9248a0b11134c79ea9957da69c5e4339
hkystar35 <32749858+hkystar35@users.noreply.github.com>
2019-05-08 14:26:30 -0600

    Create New-HyperV_AutoPilot.ps1

---
+++ PowerShell/New-HyperV_AutoPilot.ps1
@@ -0,0 +1,12 @@
+﻿$NetAdapterName = 'Wi-Fi' #<Name of Network Adapter with internet access>
+$VMSwitchName = 'AutopilotExternal'
+$VMName = 'WindowsAutopilot'
+$VHDPath = 'D:\VMs\HyperV'
+$ISO = 'D:\Software\ISO\Operating Systems\SW_DVD9_Win_Pro_Ent_Edu_N_10_1809_64-bit_English_MLF_X21-96501.ISO'
+
+New-VMSwitch -Name $VMSwitchName -NetAdapterName $NetAdapterName -AllowManagementOS $true #-WhatIf
+New-VM -Name $VMName -MemoryStartupBytes 2GB -BootDevice VHD -NewVHDPath $VHDPath\VMs\WindowsAutopilot.vhdx -Path $VHDPath\VMData -NewVHDSizeBytes 80GB -Generation 2 -Switch $VMSwitchName 
+Add-VMDvdDrive -Path $ISO -VMName $VMName 
+Start-VM -VMName $VMName
+#Set-VMMemory -VMName $VMName -StartupBytes 16GB
+Checkpoint-VM -Name $VMName -SnapshotName "Finished Windows install"
